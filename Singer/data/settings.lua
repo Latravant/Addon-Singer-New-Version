ed18ea5807526f2ef9bda01158fcc44af8f223e6
aragan <64033062+aragan@users.noreply.github.com>
2025-08-11 12:54:48 +0300

    Add files via upload

---
+++ Singer/data/settings.lua
@@ -0,0 +1,101 @@
+return {
+    ["dummy"] = L{"Army's Paeon","Shining Fantasia","Herb Pastoral",},
+    ["debuffs"] = L{"Carnage Elegy","Pining Nocturne",},
+    ["playlist"] = T{
+        ["test"] = L{"Adventurer's Dirge","Warding Round","Wind Carol II","Sword Madrigal","Valor Minuet V",},
+        ["haste"] = {'Valor Minuet IV', 'Honor March', 'Blade Madrigal', 'Valor Minuet V',  "Victory March"},
+        ["haste4"] = L{'Valor Minuet IV', 'Advancing March', 'Valor Minuet V', "Victory March"},
+        ["seg"] = L{'Valor Minuet III', 'Honor March', "Valor Minuet IV", 'Victory March', "Valor Minuet V"},
+        ["seg4"] = L{'Advancing March ', 'Victory March', "Valor Minuet IV", "Valor Minuet V"},
+        ["sortie"] = L{'Blade Madrigal', 'Honor March', 'Valor Minuet III', "Valor Minuet IV", "Valor Minuet V",},
+        ["sortie4"] = L{'Blade Madrigal', 'Honor March', "Valor Minuet IV", "Valor Minuet V",},
+        ["ambuscade"] = L{'Blade Madrigal', 'Honor March', 'Valor Minuet III', "Valor Minuet IV", "Valor Minuet V",},
+        ["ambuscade4"] = L{'Blade Madrigal', 'Honor March',  "Victory March", "Valor Minuet V",},
+        ["shinryu"] = L{'Blade Madrigal', 'Honor March',  'Valor Minuet V', 'Dark Carol', 'Herculean Etude',},
+        ["shinryu4"] = L{'Blade Madrigal', 'Honor March',  'Valor Minuet V', 'Dark Carol',},
+        --T2
+        ["Procne"] = L{"Valor Minuet IV",'Honor March',"Knight's Minne IV", 'Valor Minuet V', "Blade Madrigal",},
+        ["Procne2"] = L{"Adventurer's Dirge",'Honor March',"Sentinel's Scherzo", 'Valor Minuet V', "Blade Madrigal",},
+
+        --Odyssey V25 ADD BY Author: Aragan 
+        ["mboze"] = L{'Valor Minuet IV','Sentinel\'s Scherzo', 'Honor March', 'Earth Carol II',  'Valor Minuet V'},
+        ["mboze2"] = L{'Valor Minuet IV','Sentinel\'s Scherzo', 'Honor March', 'Blade Madrigal', 'Valor Minuet V',},
+        
+        --[[MBOZE PLAN BRD: SV Honor March, Minuet x4. HM/Minnes/Ballads for PLD and WHM. 
+          WHM will not be taking much damage at all here, 
+          but it was easier for us to apply the Ballads to both PLD and WHM at the same time. Savage Blade. ]]
+        ["xevioso"] = L{'Valor Minuet IV','Honor March',"Sentinel's Scherzo", 'Valor Minuet V', "Wind Carol II",},
+        --BRD: Honor March, Scherzo, Minuet x2, Wind Carol 1 for frontline, Honor March, Ballad x2, Minne x2 for WHM.
+        -- Melee with Rudra's/Evisceration during aura. Apply Carnage Elegy/Pining Nocturne. 
+        ["kalunga"] = L{"Adventurer's Dirge", 'Honor March', 'Valor Minuet IV', 'Valor Minuet V', 'Fire Carol II',},
+        --[[BRD: Sing slow SV songs at the start. HM Dirge Minuet x2 Fire Carol 1 for melees. Sirvente and Ballad3 the PLD. Ballad 3 / 2 the WHM. Nitro before SV wears so songs last the whole fight.
+        Savage Blade. Don't spend a ton of time rebuffing the PLD, this DPS check sucks. ]]
+        ["ngai"] = L{'Valor Minuet IV','Honor March',"Sentinel's Scherzo", 'Valor Minuet V', 'Water Carol II',},
+        --[[BRD: SV Honor March, Minuet x2, Scherzo, Water Carol2 (on MNK WAR)
+        Honor March, Scherzo, Minne, Water Carol2, Ballad (on WHM COR BRD) ]]
+        ["arebati"] = L{'Valor Minuet III', 'Honor March', 'Valor Minuet IV', 'Valor Minuet V', 'Valor Minuet II',},
+        --[[BRD: Minuet x4, Honor March (on everyone else)
+          Dirge, Honor March, Ballad, Minne x2 (on PLD)
+          COR need Scherzo ]]
+        ["ongo"] = L{'Victory March', 'Sage Etude', 'Learned Etude', "Mage's Ballad III", "Mage's Ballad II",},
+        --[[BRD: SV Victory March, Ballad x2, INT Etude x2 (on everyone)
+        Minne x3, Paeon, Sirvente (on BRD) ]]
+        ["bumba"] = L{'Valor Minuet IV',"Sentinel's Scherzo", 'Honor March',  'Valor Minuet V', 'Valor Minuet III',},
+        --[[BRD: BRD: SV Scherzo, Honor March, Minuet x3]]
+
+        --NOTE: IF U HAVE SONG Aria of Passion prime horn Loughnashade replaced Minuet
+        
+        ["cuijatender"] = L{"Sentinel's Scherzo","Victory March","Light Carol II","Valor Minuet V",},
+        ["volte"] = L{"Adventurer's Dirge","Honor March","Blade Madrigal","Sword Madrigal","Valor Minuet V",},
+        ["w3"] = L{"Adventurer's Dirge","Blade Madrigal","Honor March","Sword Madrigal","Valor Minuet V",},
+        ["melee4"] = L{"Valor Minuet V","Valor Minuet IV","Honor March","Victory March",},
+        ["xevfarm"] = L{"Valor Minuet V","Blade Madrigal","Wind Carol II","Honor March","Honor March",},
+        ["arewar"] = L{"Valor Minuet II","Valor Minuet IV","Valor Minuet V","Honor March","Valor Minuet III",},
+        ["sv5"] = L{"Valor Minuet III","Valor Minuet IV","Valor Minuet V","Honor March","Valor Minuet II",},
+        ["mboze2"] = L{"Knight's Minne IV","Knight's Minne V","Mage's Ballad II","Victory March","Mage's Ballad III",},
+        ["arebati"] = L{"Valor Minuet IV","Valor Minuet V","Knight's Minne IV","Knight's Minne V","Honor March",},
+        ["1"] = L{"Herculean Etude","Valor Minuet III","Valor Minuet IV","Valor Minuet V",},
+        ["melee"] = L{"Valor Minuet V","Valor Minuet IV","Honor March","Victory March","Valor Minuet III",},
+        ["2"] = L{"Herculean Etude","Valor Minuet III","Valor Minuet IV","Valor Minuet V","Honor March",},
+        ["mboze"] = L{"Valor Minuet V","Valor Minuet IV","Valor Minuet III","Honor March","Valor Minuet II",},
+        ["kalunga"] = L{"Valor Minuet III","Valor Minuet V","Valor Minuet IV","Honor March","Fire Carol II",},
+        ["sortie"] = L{"Valor Minuet IV","Valor Minuet V","Blade Madrigal","Honor March","Aria of Passion",},
+        ["odin"] = L{"Knight's Minne V","Honor March","Valor Minuet V","Valor Minuet IV","Valor Minuet III",},
+        ["procne"] = L{"Valor Minuet V","Valor Minuet IV","Blade Madrigal","Honor March","Knight's Minne IV",},
+        ["11"] = L{"Herculean Etude","Valor Minuet III","Valor Minuet IV","Valor Minuet V","Honor March",},
+        ["tank"] = L{"Knight's Minne V","Honor March","Foe Sirvente","Sentinel's Scherzo","Mage's Ballad III",},
+        ["ambu"] = L{"Valor Minuet V","Blade Madrigal","Honor March","Knight's Minne V","Valor Minuet IV",},
+        ["w2"] = L{"Adventurer's Dirge","Blade Madrigal","Honor March","Valor Minuet V","Knight's Minne V",},
+        ["ouryu"] = L{"Valor Minuet V","Sentinel's Scherzo","Honor March","Earth Carol",},
+        ["range"] = L{"Honor March","Valor Minuet V","Valor Minuet IV","Archer's Prelude","Valor Minuet III",},
+        -- ["ngai"] = L{"Honor March","Valor Minuet V","Blade Madrigal","Valor Minuet IV","Sentinel's Scherzo",},
+        ["ongo"] = L{"Learned Etude","Sage Etude","Mage's Ballad III","Victory March","Mage's Ballad II",},
+        ["lilmnk"] = L{"Mage's Ballad III","Blade Madrigal","Victory March","Honor March",},
+        ["alex"] = L{"Mage's Ballad III","Valor Minuet V","Honor March","Blade Madrigal",},
+        ["hast4"] = L{"Valor Minuet IV","Honor March","Valor Minuet V","Victory March",},
+        ["w3boss"] = L{"Adventurer's Dirge","Blade Madrigal","Honor March","Valor Minuet IV","Valor Minuet V",},
+        ["ody"] = L{"Valor Minuet V","Valor Minuet IV","Honor March","Victory March","Valor Minuet III",},
+        ["clear"] = L{},
+        ["cait"] = L{"Blade Madrigal","Honor March","Sentinel's Scherzo","Light Carol","Light Carol II",},
+        ["xev"] = L{"Valor Minuet V","Valor Minuet IV","Wind Carol II","Sentinel's Scherzo","Honor March",},
+        ["mage"] = L{"Honor March","Victory March","Mage's Ballad II","Mage's Ballad III",},
+        ["bumba"] = L{"Valor Minuet IV","Valor Minuet V","Knight's Minne V","Honor March","Blade Madrigal",},
+        ["pl"] = L{"Valor Minuet V","Valor Minuet IV","Adventurer's Dirge","Knight's Minne V","Honor March",},
+        ["esortie"] = L{"Mage's Ballad III","Mage's Ballad II","Honor March","Blade Madrigal","Valor Minuet IV",},
+        ["shin"] = L{"Blade Madrigal","Valor Minuet V","Sentinel's Scherzo","Honor March","Valor Minuet IV",},
+        ["lilsam"] = L{"Blade Madrigal","Sinewy Etude","Herculean Etude","Valor Minuet V","Honor March",},
+        ["ody4"] = L{"Valor Minuet V","Valor Minuet IV","Honor March","Victory March",},
+        ["gige"] = L{"Adventurer's Dirge","Honor March","Valor Minuet IV","Valor Minuet V","Valor Minuet III",},
+        ["aris"] = L{"Valor Minuet V","Valor Minuet IV","Honor March","Knight's Minne V","Knight's Minne IV",},
+        ["sortie4"] = L{"Valor Minuet V","Valor Minuet IV","Honor March","Blade Madrigal",},
+        ["meleeacc"] = L{"Valor Minuet V","Blade Madrigal","Honor March","Victory March","Valor Minuet IV",},
+        ["peach"] = L{"Honor March","Knight's Minne V","Knight's Minne IV","Army's Paeon VI","Knight's Minne III",},
+        ["meleehaste2"] = L{"Valor Minuet V","Valor Minuet IV","Honor March","Blade Madrigal","Valor Minuet III",},
+    },
+    ["clarion"] = L{},
+    ["buffs"] = T{
+    },
+    ["song"] = T{
+    },
+    ["songs"] = L{"Valor Minuet IV","Advancing March","Valor Minuet V","Victory March",},
+}
